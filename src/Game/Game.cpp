1ba3d67a97a9738dfe14f750d1ef213c531163e6
Casper <casper.eide@gmail.com>
2024-04-09 02:32:28 +0200

    v13   # Course section 13: Managing Assets     + Added AssetStore for storing assets to be shared between entities     + Added support for Textures to the AssetStore     + Implemented storing of texture id in SpriteComponent and rendering of       SpriteComponent textures in the RenderSystem     + Added first implementation of the map

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -10,10 +10,14 @@
 #include <SDL2/SDL_image.h>
 #include <glm/glm.hpp>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 Game::Game() {
     isRunning = false;
     registry = std::make_unique<Registry>();
+    assetStore = std::make_unique<AssetStore>();
     Logger::Log("Game constructor called!");
 }
 
@@ -71,22 +75,81 @@ void Game::ProcessInput() {
     }
 }
 
-void Game::Setup() {
+void Game::LoadLevel(int level) {
     // Add the systems that need to be processed in our game
     registry->AddSystem<MovementSystem>();
     registry->AddSystem<RenderSystem>();
 
+    // Adding assets to the asset store
+    assetStore->AddTexture(renderer, "tank-image", "./assets/images/tank-panther-right.png");
+    assetStore->AddTexture(renderer, "truck-image", "./assets/images/truck-ford-right.png");
+    assetStore->AddTexture(renderer, "tilemap-image", "./assets/tilemaps/jungle.png");
+
+    // Load the tilemap
+    //int mapTileX = 0;
+    //int mapTileY = 0;
+    //std::fstream mapFile;
+    //mapFile.open("./assets/tilemaps/jungle.map", std::ios::in);
+    //if(mapFile.is_open()) {
+    //    std::string mapLine;
+    //    while (getline(mapFile, mapLine)) {
+    //        mapTileX = 0;
+    //        std::stringstream mapLineStream(mapLine);
+    //        std::string mapTileIdString;
+    //        while (getline(mapLineStream, mapTileIdString, ',')) {
+    //            Entity tile = registry->CreateEntity();
+    //            int mapTileId = std::stoi(mapTileIdString);
+    //            int mapTileOffsetX = (mapTileId % 10) * 32;
+    //            int mapTileOffsetY = static_cast<int>(floor(mapTileId / 10) * 32);
+    //            //Logger::Log("Map tile id = " + std::to_string(mapTileId) + " (" + std::to_string(mapTileOffsetX) + ", " + std::to_string(mapTileOffsetY) + ") at position (" + std::to_string(mapTileX * 32) + ", " + std::to_string(mapTileY * 32) + ")");
+    //            tile.AddComponent<TransformComponent>(glm::vec2(mapTileX * 32, mapTileY * 32), glm::vec2(1.0, 1.0), 0.0);
+    //            tile.AddComponent<SpriteComponent>("jungle-image", 32, 32, mapTileOffsetX, mapTileOffsetY);
+    //            mapTileX++;
+    //        }
+    //        mapTileY++;
+    //    }
+    //    mapFile.close();
+    //}
+
+    // Load the tilemap
+    int tileSize = 32;
+    double tileScale = 2.0;
+    int mapNumCols = 25;
+    int mapNumRows = 20;
+    
+    std::fstream mapFile;
+    mapFile.open("./assets/tilemaps/jungle.map");
+
+    for (int y = 0; y < mapNumRows; y++) {
+        for (int x = 0; x < mapNumCols; x++) {
+            char ch;
+            mapFile.get(ch);
+            int srcRectY = std::atoi(&ch) * tileSize;
+            mapFile.get(ch);
+            int srcRectX = std::atoi(&ch) * tileSize;
+            mapFile.ignore();
+
+            Entity tile = registry->CreateEntity();
+            tile.AddComponent<TransformComponent>(glm::vec2(x * (tileScale * tileSize), y * (tileScale * tileSize)), glm::vec2(tileScale, tileScale), 0.0);
+            tile.AddComponent<SpriteComponent>("tilemap-image", tileSize, tileSize, srcRectX, srcRectY);
+        }
+    }
+
     // Create an entity
     Entity tank = registry->CreateEntity();
-    tank.AddComponent<TransformComponent>(glm::vec2(10.0, 30.0), glm::vec2(1.0, 1.0), 0.0);
+    tank.AddComponent<TransformComponent>(glm::vec2(10.0, 10.0), glm::vec2(1.0, 1.0), 0.0);
     tank.AddComponent<RigidBodyComponent>(glm::vec2(40.0, 0.0));
-    tank.AddComponent<SpriteComponent>(10, 10);
+    tank.AddComponent<SpriteComponent>("tank-image", 32, 32);
     
     // Create an entity
     Entity truck = registry->CreateEntity();
     truck.AddComponent<TransformComponent>(glm::vec2(50.0, 100.0), glm::vec2(1.0, 1.0), 0.0);
     truck.AddComponent<RigidBodyComponent>(glm::vec2(0.0, 50.0));
-    truck.AddComponent<SpriteComponent>(10, 50);
+    truck.AddComponent<SpriteComponent>("truck-image", 32, 32);
+}
+
+void Game::Setup() {
+    LoadLevel(1);
 }
 
 void Game::Update() {
@@ -114,7 +177,7 @@ void Game::Render() {
     SDL_RenderClear(renderer);
 
     // Invoke all the systems that need to render
-    registry->GetSystem<RenderSystem>().Update(renderer);
+    registry->GetSystem<RenderSystem>().Update(renderer, assetStore);
 
     SDL_RenderPresent(renderer);
 }
--- src/Game/Game.h
+++ src/Game/Game.h
@@ -2,6 +2,7 @@
 #define GAME_H
 
 #include "../ECS/ECS.h"
+#include "../AssetStore/AssetStore.h"
 #include <SDL2/SDL.h>
 
 const int FPS = 60;
@@ -15,6 +16,7 @@ class Game {
         SDL_Renderer* renderer;
 
         std::unique_ptr<Registry> registry;
+        std::unique_ptr<AssetStore> assetStore;
 
     public:
         Game();
@@ -22,6 +24,7 @@ class Game {
         void Initialize();
         void Run();
         void Setup();
+        void LoadLevel(int level);
         void ProcessInput();
         void Update();
         void Render();
